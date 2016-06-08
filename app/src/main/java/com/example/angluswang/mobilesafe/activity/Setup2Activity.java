6a4170666470203ae4e1c916d63464704ef7a227
AnglusWang <AnglusWang@163.com>
2016-06-08 14:13:32 +0800

    添加第二个向导页

--- app/src/main/java/com/example/angluswang/mobilesafe/activity/Setup2Activity.java
+++ app/src/main/java/com/example/angluswang/mobilesafe/activity/Setup2Activity.java
@@ -1,22 +1,25 @@
 package com.example.angluswang.mobilesafe.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.angluswang.mobilesafe.R;
 
-public class Setup1Activity extends Activity {
+public class Setup2Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup1);
+        setContentView(R.layout.activity_setup2);
     }
 
     //下一页
     public void next() {
-        startActivity(new Intent(this, Setup2Activity.class));
-        finish();
+
+    }
+
+    //上一页
+    public void previous() {
+
     }
 }
