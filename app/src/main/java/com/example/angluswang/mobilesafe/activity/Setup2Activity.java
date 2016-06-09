c950ffd01031fa1c48ce668d312e9ac4089437a3
AnglusWang <AnglusWang@163.com>
2016-06-09 17:56:08 +0800

    给第二个向导页添加手势滑动 翻页处理

--- app/src/main/java/com/example/angluswang/mobilesafe/activity/Setup2Activity.java
+++ app/src/main/java/com/example/angluswang/mobilesafe/activity/Setup2Activity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +19,8 @@ public class Setup2Activity extends Activity {
     private SettingItemView simUpdate;
     private SharedPreferences mPref;
 
+    private GestureDetector mDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +32,56 @@ public class Setup2Activity extends Activity {
         previous();
         bindSimListener();
 
+        /**
+         * 监听手势事件
+         */
+        mDetector = new GestureDetector(Setup2Activity.this,
+                new GestureDetector.SimpleOnGestureListener(){
+                    /**
+                     *
+                     * @param e1 表示滑动的起点
+                     * @param e2 表示滑动的终点
+                     * @param velocityX 表示滑动水平速度
+                     * @param velocityY 表示滑动垂直速度
+                     * @return
+                     */
+                    @Override
+                    public boolean onFling(MotionEvent e1, MotionEvent e2,
+                                           float velocityX, float velocityY) {
+
+                        //向左滑动，切换下一页
+                        if (e1.getRawX() - e2.getRawX() > 100) {
+                            startActivity(new Intent(Setup2Activity.this, Setup3Activity.class));
+                            finish();
+
+                            //两个界面切换的动画
+                            overridePendingTransition(R.anim.tran_in, R.anim.tran_out);
+
+                            return true;
+                        }
+
+                        //向右滑动，返回上一页
+                        if (e2.getRawX() - e1.getRawX() > 100) {
+                            startActivity(new Intent(Setup2Activity.this, Setup1Activity.class));
+                            finish();
+
+                            overridePendingTransition(R.anim.tran_previous_in,
+                                    R.anim.tran_previous_out);
+
+                            return true;
+                        }
+
+                        return super.onFling(e1, e2, velocityX, velocityY);
+                    }
+                });
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //委托手势识别器处理触摸事件
+        mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 
     /**
