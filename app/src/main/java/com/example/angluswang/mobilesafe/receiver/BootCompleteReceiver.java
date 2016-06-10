4d6ecfab5979943061d25b679edd2f769885a710
AnglusWang <AnglusWang@163.com>
2016-06-10 20:31:39 +0800

    添加给安全号码发送报警短信

--- app/src/main/java/com/example/angluswang/mobilesafe/receiver/BootCompleteReceiver.java
+++ app/src/main/java/com/example/angluswang/mobilesafe/receiver/BootCompleteReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 
@@ -25,12 +26,19 @@ public class BootCompleteReceiver extends BroadcastReceiver {
             if (!TextUtils.isEmpty(sim)) {
                 TelephonyManager tm = (TelephonyManager) context
                         .getSystemService(Context.TELEPHONY_SERVICE);
-                String currentSim = tm.getSimSerialNumber();
+                String currentSim = tm.getSimSerialNumber();    //拿到当前Sim卡信息
 
                 if (sim.equals(currentSim)) {
                     System.out.println("手机安全~");
                 }else {
                     System.out.println("Sim卡变更，发送报警短信！！！");
+
+                    String phone = pref.getString("safe_phone", "");  //读取安全号码
+
+                    //调用Sim卡管理器
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phone, null,
+                            "Sim card is changed!!!", null, null);
                 }
 
             }
