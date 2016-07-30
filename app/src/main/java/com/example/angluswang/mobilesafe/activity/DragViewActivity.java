58874bdaaaa799b546f91ebf0c5389723b6ab6f5
AnglusWang <AnglusWang@163.com>
2016-07-30 22:08:46 +0800

    设置归属地位置完成，及添加半透明效果

--- app/src/main/java/com/example/angluswang/mobilesafe/activity/DragViewActivity.java
+++ app/src/main/java/com/example/angluswang/mobilesafe/activity/DragViewActivity.java
@@ -2,6 +2,7 @@ package com.example.angluswang.mobilesafe.activity;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -50,6 +51,20 @@ public class DragViewActivity extends Activity {
         // 以下方法不能使用，因为没有测量完不能布局
 //        imgDrag.layout(lastX, lastY, lastX + imgDrag.getWidth(), lastY + imgDrag.getHeight());
 
+        // 获取屏幕宽高
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+        final int winWidth = point.x;
+        final int winHight = point.y;
+        
+        if (lastY > winHight / 2) { //上边文本框显示, 下边文本框隐藏
+            tvTop.setVisibility(View.VISIBLE);
+            tvBottom.setVisibility(View.INVISIBLE);
+        } else {
+            tvTop.setVisibility(View.INVISIBLE);
+            tvBottom.setVisibility(View.VISIBLE);
+        }
+
         RelativeLayout.LayoutParams params =
                 (RelativeLayout.LayoutParams) imgDrag.getLayoutParams();//获取布局对象
         params.leftMargin = lastX;  // 设置左边距
@@ -79,6 +94,19 @@ public class DragViewActivity extends Activity {
                         int t = imgDrag.getTop() + dy;
                         int r = imgDrag.getRight() + dx;
                         int b = imgDrag.getBottom() + dy;
+
+                        // 判断是否超出屏幕外，注意状态栏
+                        if (l < 0 | r > winWidth | t < 0 | b > winHight - 60) {
+                            break;
+                        }
+
+                        if (t > winHight / 2) { //上边文本框显示, 下边文本框隐藏
+                            tvTop.setVisibility(View.VISIBLE);
+                            tvBottom.setVisibility(View.INVISIBLE);
+                        } else {
+                            tvTop.setVisibility(View.INVISIBLE);
+                            tvBottom.setVisibility(View.VISIBLE);
+                        }
                         imgDrag.layout(l, t, r, b);
 
                         // 4. 重新初始化起点坐标
@@ -88,7 +116,7 @@ public class DragViewActivity extends Activity {
                     case MotionEvent.ACTION_UP:
                         // 5. 手指抬起后，记录view 的坐标
                         mPref.edit().putInt("lastX", imgDrag.getLeft())
-                                .putInt("lastY", imgDrag.getTop()).commit();
+                                .putInt("lastY", imgDrag.getTop()).apply();
                         break;
                 }
                 return true;
