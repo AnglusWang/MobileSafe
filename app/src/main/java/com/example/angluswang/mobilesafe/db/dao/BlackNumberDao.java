01f7b9db62802187a5abc9dbbf39d1dbfea26df5
AnglusWang <AnglusWang@163.com>
2016-08-01 09:08:38 +0800

    黑名单 数据库 实现增删改查

--- app/src/main/java/com/example/angluswang/mobilesafe/db/dao/BlackNumberDao.java
+++ app/src/main/java/com/example/angluswang/mobilesafe/db/dao/BlackNumberDao.java
@@ -1,6 +1,14 @@
 package com.example.angluswang.mobilesafe.db.dao;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.angluswang.mobilesafe.entity.BlackNumberInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Jeson on 2016/8/1.
@@ -21,33 +29,78 @@ public class BlackNumberDao {
      * @return
      */
     public boolean add(String number, String mode) {
-        return false;
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("number", number);
+        cv.put("mode", mode);
+        long rowId = db.insert("black_number", null, cv);
+        db.close();
+        return rowId != -1;
     }
 
     /**
      * 把号码移黑名单
+     *
      * @param number
      * @return
      */
     public boolean delete(String number) {
-        return false;
+        SQLiteDatabase db = helper.getWritableDatabase();
+        int rowNumber = db.delete("black_number", "number=?", new String[]{number});
+        db.close();
+        return rowNumber == 1;
     }
 
     /**
      * 改变拦截模式
+     *
      * @param number
      * @return
      */
-    public boolean changeMode(String number) {
-        return false;
+    public boolean changeMode(String number, String mode) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("mode", mode);
+        int rowNumber = db.update("black_number", cv, "number=?", new String[]{number});
+        db.close();
+        return rowNumber != 0;
     }
 
     /**
-     * 查找黑名单
+     * 查找黑名单的拦截模式
+     *
      * @return
      */
-    public String queryNumber() {
-        return null;
+    public String findNumber(String number) {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        String mode = "";
+        Cursor cursor = db.query("black_number", new String[]{mode},
+                "number=?", new String[]{number}, null, null, null);
+        if (cursor.moveToNext()) {
+            mode = cursor.getString(0);
+        }
+        cursor.close();
+        db.close();
+        return mode;
+    }
+
+    /**
+     * 查询所有的黑名单
+     */
+    public List<BlackNumberInfo> findAllNumber() {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        List<BlackNumberInfo> blackNumInfos = new ArrayList<>();
+        Cursor cursor = db.query("black_number", new String[]{"number", "mode"},
+                null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            BlackNumberInfo info = new BlackNumberInfo();
+            info.setNumber(cursor.getString(0));
+            info.setMode(cursor.getString(1));
+            blackNumInfos.add(info);
+        }
+        cursor.close();
+        db.close();
+        return blackNumInfos;
     }
 
 }
