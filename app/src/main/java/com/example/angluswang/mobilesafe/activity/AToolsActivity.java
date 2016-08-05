941bf04e427b49ea1d3ce9b15cd141b0861954d7
AnglusWang <AnglusWang@163.com>
2016-08-05 09:52:47 +0800

    短信备份 --- 代码修改优化 及 添加进度对话框

--- app/src/main/java/com/example/angluswang/mobilesafe/activity/AToolsActivity.java
+++ app/src/main/java/com/example/angluswang/mobilesafe/activity/AToolsActivity.java
@@ -1,17 +1,21 @@
 package com.example.angluswang.mobilesafe.activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.angluswang.mobilesafe.R;
 import com.example.angluswang.mobilesafe.utils.SmsUtils;
+import com.example.angluswang.mobilesafe.utils.UIUtils;
 
 /**
  * 高级工具
  */
 public class AToolsActivity extends Activity {
+
+    private ProgressDialog pd; // 短信备份进度框
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,7 @@ public class AToolsActivity extends Activity {
 
     /**
      * 电话号码归属地查询
+     *
      * @param view
      */
     public void numberAddressQuery(View view) {
@@ -29,9 +34,32 @@ public class AToolsActivity extends Activity {
 
     /**
      * 短信备份
+     *
      * @param view
      */
     public void messagingBackup(View view) {
-        boolean result = SmsUtils.backUp(this);
+
+        // 初始化一个进度条的对话框
+        pd = new ProgressDialog(this);
+        pd.setTitle("提示");
+        pd.setMessage("正在执行操作，请等待。。。");
+        pd.show();
+
+        new Thread() {
+            @Override
+            public void run() {
+                boolean result = SmsUtils.backUp(AToolsActivity.this, pd);
+                if (result) {
+//                    Looper.prepare(); // 取消息
+//                    Toast.makeText(AToolsActivity.this, "短信备份成功",
+//                            Toast.LENGTH_SHORT).show();
+//                    Looper.loop();
+                    UIUtils.showToast(AToolsActivity.this, "短信备份成功");
+                } else {
+                    UIUtils.showToast(AToolsActivity.this, "短信备份失败");
+                }
+                pd.dismiss();
+            }
+        };
     }
 }
--- app/src/main/java/com/example/angluswang/mobilesafe/utils/SmsUtils.java
+++ app/src/main/java/com/example/angluswang/mobilesafe/utils/SmsUtils.java
@@ -1,10 +1,12 @@
 package com.example.angluswang.mobilesafe.utils;
 
+import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.SystemClock;
 import android.util.Log;
 import android.util.Xml;
 
@@ -29,7 +31,7 @@ public class SmsUtils {
      * 使用内容观察者
      * 3 写短信(写到sd卡)
      */
-    public static boolean backUp(Context context) {
+    public static boolean backUp(Context context, ProgressDialog pd) {
 
         // 判断 SD 卡状态
         if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
@@ -40,6 +42,12 @@ public class SmsUtils {
             // type = 1 接收短信    type = 2 发送短信
             Cursor cursor = resolver.query(uri,
                     new String[]{"address", "date", "type", "body"}, null, null, null);
+
+            int count = cursor.getCount();// 获取总的短信数
+            // 设置短息备份进度框的一些属性
+            pd.setMax(count);
+            // 进度条默认为 0
+            int process = 0;
 
             // 写文件
             try {
@@ -56,6 +64,8 @@ public class SmsUtils {
                 serializer.startDocument("utf-8", true);
                 // 设置开始的节点 第一个参数是命名空间。第二个参数是节点的名字
                 serializer.startTag(null, "smss");
+                // 设置 smms 节点上的属性值
+                serializer.attribute(null, "size", String.valueOf(count));
 
                 // 游标往下面进行移动
                 while (cursor.moveToNext()) {
@@ -68,24 +78,25 @@ public class SmsUtils {
                     serializer.startTag(null, "sms");
 
                     serializer.startTag(null, "address");
-
                     serializer.text(cursor.getString(0)); // 设置文本的内容
-
                     serializer.endTag(null, "address");
 
                     serializer.startTag(null, "date");
-
                     serializer.text(cursor.getString(1));
-
                     serializer.endTag(null, "date");
 
                     serializer.startTag(null, "type");
-
                     serializer.text(cursor.getString(2));
-
                     serializer.endTag(null, "type");
 
                     serializer.startTag(null, "body");
+                    serializer.text(cursor.getString(3));
+                    serializer.endTag(null, "body");
+
+                    process ++; // 序列化完成一条短信后，自加 1
+                    pd.setProgress(process);
+
+                    SystemClock.sleep(200); // 减慢备份数据，便于观察进度框
                 }
 
                 serializer.endTag(null, "smss");
