c9261277c47b492c7e45a2cde898a395a28f1956
AnglusWang <AnglusWang@163.com>
2016-08-09 09:23:26 +0800

    应用程序锁 --- 看门狗 能判断用户当前运行的程序是否在 程序锁数据库中

--- app/src/main/java/com/angluswang/mobilesafe/service/WatchDogService.java
+++ app/src/main/java/com/angluswang/mobilesafe/service/WatchDogService.java
@@ -1,8 +1,14 @@
 package com.angluswang.mobilesafe.service;
 
+import android.app.ActivityManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
+
+import com.angluswang.mobilesafe.dao.AppLockDao;
+
+import java.util.List;
 
 /**
  * Created by AnglusWang on 2016/8/9.
@@ -10,6 +16,9 @@ import android.os.IBinder;
  */
 
 public class WatchDogService extends Service {
+
+    private ActivityManager am; // 进程管理器
+    private AppLockDao dao;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -20,6 +29,36 @@ public class WatchDogService extends Service {
     public void onCreate() {
         super.onCreate();
 
+        // 获得进程管理器
+        am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        dao = new AppLockDao(this);
+        /**
+         * 1. 获得当前任务栈
+         * 2. 得到最上面的进程
+         */
+        startWatchDog();
+    }
+
+    private void startWatchDog() {
+        new Thread(){
+            @Override
+            public void run() {
+                while (true) {
+                    // 为避免发生阻塞，开启子线程
+                    // 获得当前正在运行的任务栈
+                    List<ActivityManager.RunningTaskInfo> runningTasks = am.getRunningTasks(2);
+                    // 获得最上面的进程
+                    ActivityManager.RunningTaskInfo taskInfo = runningTasks.get(0);
+
+                    String packageName = taskInfo.topActivity.getPackageName();
+                    if (dao.find(packageName)) {
+                        Log.i("app_lockInfo ===== ", packageName + "在程序锁数据库中");
+                    } else {
+                        Log.i("app_lockInfo ===== ", packageName + "查无此果~~~");
+                    }
+                }
+            }
+        }.start();
     }
 
     @Override
