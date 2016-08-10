95c8ef6796319184f7cf52a9441bb3dabe3d9189
AnglusWang <AnglusWang@163.com>
2016-08-10 22:39:39 +0800

    缓存清理 --- update

--- app/src/main/java/com/angluswang/mobilesafe/activity/activity/CleanCacheActivity.java
+++ app/src/main/java/com/angluswang/mobilesafe/activity/activity/CleanCacheActivity.java
@@ -1,6 +1,9 @@
 package com.angluswang.mobilesafe.activity.activity;
 
 import android.app.Activity;
+import android.content.pm.IPackageStatsObserver;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -8,6 +11,9 @@ import com.angluswang.mobilesafe.R;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ContentView;
 import com.lidroid.xutils.view.annotation.ViewInject;
+
+import java.lang.reflect.Method;
+import java.util.List;
 
 /**
  * Created by AnglusWang on 2016/8/9.
@@ -30,5 +36,40 @@ public class CleanCacheActivity extends Activity {
 
     private void initView() {
 
+        PackageManager packageManager = getPackageManager();
+        /**
+         * 接收2个参数
+         * 第一个参数接收一个包名
+         * 第二个参数接收aidl的对象
+         */
+//		  * @hide
+//		     */
+//		    public abstract void getPackageSizeInfo(String packageName,
+//		            IPackageStatsObserver observer);
+//		packageManager.getPackageSizeInfo();
+
+        //安装到手机上面所有的应用程序
+        List<PackageInfo> installedPackages = packageManager.getInstalledPackages(0);
+
+        for (PackageInfo packageInfo : installedPackages) {
+            getCacheSize(packageInfo);
+        }
+    }
+
+    /**
+     * 获取 应用缓存的大小（通过反射实现）
+     *
+     * @param packageInfo
+     */
+    private void getCacheSize(PackageInfo packageInfo) {
+        try {
+            Class<?> aClass = getClassLoader().loadClass("PackageManager");
+            //通过反射获取到当前的方法
+            Method method = aClass.getDeclaredMethod("getPackageSizeInfo",
+                    String.class, IPackageStatsObserver.class);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
